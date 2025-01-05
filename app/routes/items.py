213f65b27212da4ade4c892bdbede0ac23311c72
Chaa Loftin <chaamail@yahoo.com>
2025-01-05 16:34:13 -0500

    added path annotations to users and items routes

--- app/routes/items.py
+++ app/routes/items.py
@@ -1,7 +1,7 @@
 import re
 from typing import Annotated, Any, Dict, List, Union
 from xml.etree.ElementInclude import include
-from fastapi import APIRouter, Query
+from fastapi import APIRouter, Path, Query
 
 router = APIRouter()
 
@@ -14,7 +14,7 @@ async def get_all_items(
     q_list: Annotated[Union[List[str], None], Query(alias="q-list", deprecated=True)] = None,
     hidden_q: Annotated[Union[str, None], Query(deprecated=True, include_in_schema=False)] = None
 ) -> Dict[str, Union[List[Item], str, List[str]]]:
-    results: dict = {"items": [Item(id="a", name="Spam", price=1.0)], "required_q": required_q}
+    results: dict = {"items": [Item(id=1, name="Spam", price=1.0)], "required_q": required_q}
 
     if q:
         results.update({"q": q})
@@ -24,7 +24,10 @@ async def get_all_items(
     return results
 
 @router.get("/{id}")
-async def get_item(id: str, include_attributes: Annotated[Union[List[str], None], Query(max_length=3)] = None):
+async def get_item(
+    id: Annotated[int, Path(ge=1)],
+    include_attributes: Annotated[Union[List[str], None], Query(max_length=3)] = None
+):
     result: dict = {
         "item": Item(id=id, name="soup", price=5.0).model_dump(),
     }
--- app/routes/users.py
+++ app/routes/users.py
@@ -1,6 +1,6 @@
-from typing import Any, Dict, Union
+from typing import Annotated, Any, Dict, Union
 from xml.etree.ElementInclude import include
-from fastapi import APIRouter
+from fastapi import APIRouter, Path, Query
 from app.schemas import User
 
 router = APIRouter()
@@ -11,13 +11,21 @@ async def get_user_me() -> User:
         name="My user"
     )
 
-@router.get("/{user_id}")
-async def get_user(user_id, min_age: Union[int,  None] = None, max_age: Union[int, None] = None) -> User:
+@router.get("/{id}")
+async def get_user(
+    id: Annotated[int, Path(title="The id of the user", ge=1)],
+    min_age: Annotated[Union[int,  None], Query(ge=1)] = None,
+    max_age: Annotated[Union[int, None], Query(ge=1)] = None
+    ) -> User:
     return User(name="Jane Doe", age=20)
 
-@router.put("/{user_id}")
-async def update_user(user_id: int, user: User, include_deleted: bool = False) -> Dict[str, Any]:
-    result = {"user_id": user_id, **user.model_dump()}
+@router.put("/{id}")
+async def update_user(
+    id: Annotated[int, Path(title="The id of  the user", ge=1)],
+    user: User,
+    include_deleted: Annotated[bool, Query()] = False
+) -> Dict[str, Any]:
+    result = {"id": id, **user.model_dump()}
 
     if include_deleted:
         result.update({"is_deleted": True})
